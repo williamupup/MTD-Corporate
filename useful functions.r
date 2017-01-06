2570ee050cb1e54eba4be50d0acabffbbd6243d0
Haonan Guo <ghn371961285@gmail.com>
2017-01-05 23:46:08 -0500

    Update useful functions.r

--- useful functions.r
+++ useful functions.r
@@ -1,20 +1,24 @@
-rf <- function(platform = NULL, dealer = "", year = NULL , state = "", PDI = F){
+#1. Rawdata Function, under the condition that rawdata has already benn loaded
+rf <- function(platform = NULL, dealer = "", year = NULL , state = "", PDI = ""){
     asd <- rawdata
     #platform !!!!!!!!!!!!!!!!!! when it's null asd  = null
     if (is.null(platform) == F) {
-        pos1 <- substr(platform, 1,2)
+        pos12 <- substr(platform, 1,2)
         pos3 <- substr(platform,3,3)
-        asd <- asd[asd$POS1.2 == substr(platform,1,2) & asd$POS5 == substr(platform,3,3),]
+        asd <- asd[asd$POS1.2 == pos12,]
+        if (pos3 != "") {
+            asd <- asd[asd$POS5 == pos3,]
+        }
     }
     
-    #dealer!!!!!!!!!!!!!!!!!!!! when dealer is null, asd will lose 240 rows, some dealer name has a <> in it so can't identify
+    #dealer!!!!!!!!!!!!!!!!!!!! when dealer is null, asd will lose 233 rows, some dealer name has a <> in it so can't identify
     #stringarray <- strsplit(dealer,"")
     #condicion <- 
     asd <- asd[grep(dealer, asd$Repair.Dealer.Name, ignore.case = T),]
     
-    #year!!!!!!!!!!!!!!!!!still no default value
+    #year
     if (is.null(year) == F){
-        asd <- asd[asd$Failure.Calendar.Year == year,]
+        asd <- asd[asd$Production.Calendar.Year %in% year,]
     }
     
     #state
@@ -23,7 +27,29 @@ rf <- function(platform = NULL, dealer = "", year = NULL , state = "", PDI = F){
     #PDI
     if (PDI == T) {
         asd <- asd[grep("[Pp][Dd][Ii]", asd$Failure.Description),]
-    }
+    } else
+    if (PDI == F) {
+        asd <- asd[-grep("[Pp][Dd][Ii]", asd$Failure.Description),]
+    } else
+        asd <- asd
     return(asd)
     
+}
+
+#2. Sales Function, under the condition that sales data has alreadt been loaded
+sf <- function(dealer = "", year = NULL, platform = ""){
+    asd <- sales
+    
+    #dealer
+    asd <- asd[grep(dealer, asd$Customer.Name, ignore.case = T),]
+    
+    #year
+    if (is.null(year) == F){
+        asd <- asd[asd$Shipment.Fiscal.Year == year,]
+    }
+    
+    #platform
+    asd <- asd[grep(platform, asd$Item.Platform, ignore.case = T),]
+    
+    return(asd)
 }
