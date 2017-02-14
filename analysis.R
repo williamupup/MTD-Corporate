0a9f525f80ad644b861cc1781df8798df4de7db9
Haonan Guo <ghn371961285@gmail.com>
2017-02-14 16:07:48 -0500

    Update analysis.R

--- analysis.R
+++ analysis.R
@@ -50,14 +50,15 @@ sum(abs(NPDIsalesUV$Shipped.Units))     #421, there are negative values I don't 
 sum(abs(PDIsalesUV$Shipped.Units))      #41, there are negative values I don't know why
 NPDIwarUV <- rf(platform = "37", year = 2012:2016, PDI = F)
 PDIwarUV <- rf(platform = "37", year = 2012:2016, PDI = T)
-sum(as.numeric(levels(NPDIwarUV$Total.Claim.Dollars))[NPDIwarUV$Total.Claim.Dollars], na.rm = T)   #111655
+sum(as.numeric(levels(NPDIwarUV$Total.Claim.Dollars))[NPDIwarUV$Total.Claim.Dollars], na.rm = T)   #118273.1
 sum(as.numeric(levels(PDIwarUV$Total.Claim.Dollars))[PDIwarUV$Total.Claim.Dollars], na.rm = T) + 
-    + (41 - nrow(PDIwarUV[PDIwarUV$Total.Failure.Cost == 50,])) * 50    #3517.31
-111655/421
-3517.31/41
+    + (41 - nrow(PDIwarUV[PDIwarUV$Total.Failure.Cost == 50,])) * 50    #3532.29
+118273.1/421
+3532.29/41
 
 ###################################################################################################
 #A Function to automaticlly generate necessary information
+#1111111111111111111111111111111111111111111
 info <- function(platform, pos,money){
     #Sales
     sales <- sf(platform = platform)
@@ -74,10 +75,79 @@ info <- function(platform, pos,money){
     PDItotalcost <- sum(as.numeric(levels(PDIwar$Total.Claim.Dollars))[PDIwar$Total.Claim.Dollars], na.rm = T) + 
     + (PDIno - nrow(PDIwar[PDIwar$Total.Failure.Cost == money,])) * money
     #asd <- c(NPDItotalcost/NPDIno, PDItotalcost/PDIno)
-    asd <- c(year,NPDIno,PDIno,NPDItotalcost,PDItotalcost,NPDItotalcost/NPDIno, PDItotalcost/PDIno)
+    asd <- c(year[1], year[length(year)],NPDIno,PDIno,NPDItotalcost,PDItotalcost,NPDItotalcost/NPDIno, PDItotalcost/PDIno)
     return(asd)
 }
 
+infoii <- function(pos, money, name){
+    NPDIwar <- rf(platform = pos, year = 2012:2016, PDI = F)
+    PDIwar <- rf(platform = pos, year = 2012:2016, PDI = T)
+    PDIno <- nrow(PDIwar[PDIwar$Total.Failure.Cost == money,])
+    NPDItotalcost <- sum(as.numeric(levels(NPDIwar$Total.Claim.Dollars))[NPDIwar$Total.Claim.Dollars], na.rm = T)   
+    PDItotalcost <- sum(as.numeric(levels(PDIwar$Total.Claim.Dollars))[PDIwar$Total.Claim.Dollars], na.rm = T) 
+    asd <- c(name, PDIno, NPDItotalcost, PDItotalcost)
+    return(asd)
+}
 
+#22222222222222222222222222222222222222222
+info <- function(platform, pos,money){
+    #Sales
+    sales <- sf(platform = platform)
+    NPDIsales <- sales[sales$SBU.Division == 81,]
+    PDIsales <- sales[sales$SBU.Division == 73,]
+    year <- range(NPDIsales$Shipment.Fiscal.Year)[1]:range(NPDIsales$Shipment.Fiscal.Year)[2]
+    NPDIno <- sum(abs(NPDIsales$Shipped.Units))    
+    PDIno <- sum(abs(PDIsales$Shipped.Units)) 
+    
+    #Warranty Claims
+    NPDIwar <- rf(platform = pos, year = year, PDI = F)
+    PDIwar <- rf(platform = pos, year = year, PDI = T)
+    NPDItotalcost <- sum(NPDIwar$Total.Failure.Cost, na.rm = T)     
+    PDItotalcost <- sum(PDIwar$Total.Failure.Cost, na.rm = T) + 
+        + (PDIno - nrow(PDIwar[PDIwar$Total.Failure.Cost == money,])) * money
+    #asd <- c(NPDItotalcost/NPDIno, PDItotalcost/PDIno)
+    asd <- c(year[1], year[length(year)],NPDIno,PDIno,NPDItotalcost,PDItotalcost,NPDItotalcost/NPDIno, PDItotalcost/PDIno)
+    return(asd)
+}
+
+infoii <- function(pos, money, name){
+    NPDIwar <- rf(platform = pos, year = 2012:2016, PDI = F)
+    PDIwar <- rf(platform = pos, year = 2012:2016, PDI = T)
+    NPDIno <- length(unique(NPDIwar$Serial.Number))
+    #PDIno <- nrow(PDIwar[PDIwar$Total.Failure.Cost == money,])
+    PDIno <- length(unique(PDIwar$Serial.Number))
+    NPDItotalcost <- sum(NPDIwar$Total.Failure.Cost, na.rm = T)   
+    PDItotalcost <- sum(PDIwar$Total.Failure.Cost, na.rm = T)  
+    asd <- c(name, NPDIno, PDIno, NPDItotalcost, PDItotalcost)
+    return(asd)
+}
+
+#This function applies for UV, RZT, 2-Force, Commercial Z, 
 ######################################################################################################
-#Exclusively for Commercial Z
+#Still lake NPDI total cost
+residentialzinfo <- info(platform = "residential", pos = "17C", money = 25)[c(3,5,7)]
+residentialz <- infoii(pos = "17C", money = 25, name = "RZT")
+residentialz <- rbind(residentialz, infoii(pos = "172", money = 25, name = "RZT"))
+residentialz <- rbind(residentialz, infoii(pos = "173", money = 25, name = "Z-Force"))
+residentialz <- rbind(residentialz, infoii(pos = "175", money = 25, name = "Z-Force"))
+residentialz <- rbind(residentialz, infoii(pos = "17D", money = 25, name = "Z-Force"))
+residentialz <- as.data.frame(residentialz)
+colnames(residentialz) <- c("Type", "PDI Number", "NPDI Total Cost", "PDI Total Cost")
+
+commercialzinfo <- info(platform = "commercial", pos = "532", money = 30)[c(3,5,7)]
+commercialz <- infoii(pos = "53E", money = 30, name = "Pro Z")
+commercialz <- rbind(commercialz,infoii(pos = "532", money = 30, name = "Tank") )
+commercialz <- rbind(commercialz,infoii(pos = "535", money = 30, name = "Commercial Z") )
+commercialz <- rbind(commercialz,infoii(pos = "53D", money = 30, name = "Commercial Z") )
+commercialz <- as.data.frame(commercialz)
+colnames(commercialz) <-  c("Type", "PDI Number", "NPDI Total Cost", "PDI Total Cost")
+
+lawntractorinfo <- info(platform = "Lawn Tractor", pos = "131", money = 25)[c(3,5,7)]
+lawntractor <- infoii(pos = "137", money = 25, name = "LT5")
+lawntractor <- rbind(lawntractor, infoii(pos = "13A", money = 25, name = "NX15"))
+lawntractor <- rbind(lawntractor, infoii(pos = "139", money = 25, name = "NX9"))
+lawntractor <- rbind(lawntractor, infoii(pos = "131", money = 25, name = "RT99GT & Series 1500"))
+lawntractor <- rbind(lawntractor, infoii(pos = "132", money = 25, name = "CR12 & Series 2000"))
+lawntractor <- as.data.frame(lawntractor)
+colnames(lawntractor) <-  c("Type", "PDI Number", "NPDI Total Cost", "PDI Total Cost")
+
